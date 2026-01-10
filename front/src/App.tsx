e7528b44526c0974c8d4ce15be3d8ef0dea47628
Remi Poulenard <remi.poulenard@epitech.eu>
2026-01-10 10:51:23 +0100

    fix: router replace to /select

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
@@ -8,6 +8,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
+        <Route path='/' element={<Navigate to='/select' replace />} />
         <Route path='/home' element={<Home />} />
         <Route path='/select' element={<WorkoutSelector />} />
         <Route path='*' element={<NotFound />} />
